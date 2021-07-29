c1209c8cc9326de74917d21700df8214cee1669b
Janez Bozic <janez.bozic1@gmail.com>
2021-07-29 20:34:44 +0200

    Code cleanup

--- NdkBinderClient/src/main/cpp/tests/imagetests/imagetests.cpp
+++ NdkBinderClient/src/main/cpp/tests/imagetests/imagetests.cpp
@@ -40,9 +40,9 @@ void brightness(AndroidBitmapInfo* info, void* pixels, float brightnessValue, bo
     uint32_t* line;
 
     if (perf) {
+        #pragma clang loop perforate (enable)
         for (yy = 0; yy < info->height; yy++) {
             line = (uint32_t *) ((char*)pixels+(yy*info->stride));
-            #pragma clang loop perforate (enable)
             for (xx = 0; xx < info->width; xx++) {
 
                 //extract the RGB values from the pixel
@@ -110,48 +110,48 @@ void edgeDetection(AndroidBitmapInfo* infoBase, void* pixelsBase, AndroidBitmapI
                                                    ((yy + 1) * infoBase->stride));
             #pragma clang loop perforate (enable)
             for (xx = 0; xx < infoBase->width; xx++) {
-                if (xx > 0 && xx < infoBase->width - 1) {
-                    int a = (redFun(prevLineBase[xx - 1]) + blueFun(prevLineBase[xx - 1])
-                             + greenFun(prevLineBase[xx - 1])) / 3;
-                    int b = (redFun(prevLineBase[xx]) + blueFun(prevLineBase[xx])
-                             + greenFun(prevLineBase[xx])) / 3;
-                    int c = (redFun(prevLineBase[xx + 1]) + blueFun(prevLineBase[xx + 1])
-                             + greenFun(prevLineBase[xx + 1])) / 3;
+                if (xx == 0 || xx == infoBase->height - 1)
+                    continue;
+                int a = (redFun(prevLineBase[xx - 1]) + blueFun(prevLineBase[xx - 1])
+                         + greenFun(prevLineBase[xx - 1])) / 3;
+                int b = (redFun(prevLineBase[xx]) + blueFun(prevLineBase[xx])
+                         + greenFun(prevLineBase[xx])) / 3;
+                int c = (redFun(prevLineBase[xx + 1]) + blueFun(prevLineBase[xx + 1])
+                         + greenFun(prevLineBase[xx + 1])) / 3;
 
-                    int d = (blueFun(lineBase[xx - 1]) + greenFun(lineBase[xx - 1])
-                             + redFun(lineBase[xx - 1])) / 3;
-                    int e = (blueFun(lineBase[xx]) + greenFun(lineBase[xx])
-                             + redFun(lineBase[xx])) / 3;
-                    int f = (blueFun(lineBase[xx + 1]) + greenFun(lineBase[xx + 1])
-                             + redFun(lineBase[xx + 1])) / 3;
+                int d = (blueFun(lineBase[xx - 1]) + greenFun(lineBase[xx - 1])
+                         + redFun(lineBase[xx - 1])) / 3;
+                int e = (blueFun(lineBase[xx]) + greenFun(lineBase[xx])
+                         + redFun(lineBase[xx])) / 3;
+                int f = (blueFun(lineBase[xx + 1]) + greenFun(lineBase[xx + 1])
+                         + redFun(lineBase[xx + 1])) / 3;
 
-                    int g = (greenFun(nextLineBase[xx - 1]) + redFun(nextLineBase[xx - 1])
-                             + blueFun(nextLineBase[xx - 1])) / 3;
-                    int h = (greenFun(nextLineBase[xx]) + redFun(nextLineBase[xx])
-                             + blueFun(nextLineBase[xx])) / 3;
-                    int i = (greenFun(nextLineBase[xx + 1]) + redFun(nextLineBase[xx + 1])
-                             + blueFun(nextLineBase[xx + 1])) / 3;
+                int g = (greenFun(nextLineBase[xx - 1]) + redFun(nextLineBase[xx - 1])
+                         + blueFun(nextLineBase[xx - 1])) / 3;
+                int h = (greenFun(nextLineBase[xx]) + redFun(nextLineBase[xx])
+                         + blueFun(nextLineBase[xx])) / 3;
+                int i = (greenFun(nextLineBase[xx + 1]) + redFun(nextLineBase[xx + 1])
+                         + blueFun(nextLineBase[xx + 1])) / 3;
 
-                    int matrix[3][3] = {a, b, c, d, e, f, g, h, i};
+                int matrix[3][3] = {a, b, c, d, e, f, g, h, i};
 
-                    int sumx = 0;
-                    int sumy = 0;
+                int sumx = 0;
+                int sumy = 0;
 
-                    for (int s = 0; s < 3; s++) {
-                        for (int t = 0; t < 3; t++) {
-                            sumx = sumx + (matrix[s][t] * kx[s][t]);
-                            sumy = sumy + (matrix[s][t] * ky[s][t]); /* use ky, not kx */
-                        }
+                for (int s = 0; s < 3; s++) {
+                    for (int t = 0; t < 3; t++) {
+                        sumx = sumx + (matrix[s][t] * kx[s][t]);
+                        sumy = sumy + (matrix[s][t] * ky[s][t]); /* use ky, not kx */
                     }
-
-                    int newValue = rgb_clamp(sqrt(pow(sumx, 2) + pow(sumy, 2)));
-
-                    // set the new pixel back in
-                    lineToChange[xx] = (lineBase[xx] & 0xFF000000) |
-                                       ((newValue << 16) & 0x00FF0000) |
-                                       ((newValue << 8) & 0x0000FF00) |
-                                       (newValue & 0x000000FF);
                 }
+
+                int newValue = rgb_clamp(sqrt(pow(sumx, 2) + pow(sumy, 2)));
+
+                // set the new pixel back in
+                lineToChange[xx] = (lineBase[xx] & 0xFF000000) |
+                                   ((newValue << 16) & 0x00FF0000) |
+                                   ((newValue << 8) & 0x0000FF00) |
+                                   (newValue & 0x000000FF);
             }
         }
     }
@@ -166,48 +166,48 @@ void edgeDetection(AndroidBitmapInfo* infoBase, void* pixelsBase, AndroidBitmapI
             uint32_t *nextLineBase = (uint32_t *) ((char *) pixelsBase +
                                                    ((yy + 1) * infoBase->stride));
             for (xx = 0; xx < infoBase->width; xx++) {
-                if (xx > 0 && xx < infoBase->width - 1) {
-                    int a = (redFun(prevLineBase[xx - 1]) + blueFun(prevLineBase[xx - 1])
-                             + greenFun(prevLineBase[xx - 1])) / 3;
-                    int b = (redFun(prevLineBase[xx]) + blueFun(prevLineBase[xx])
-                             + greenFun(prevLineBase[xx])) / 3;
-                    int c = (redFun(prevLineBase[xx + 1]) + blueFun(prevLineBase[xx + 1])
-                             + greenFun(prevLineBase[xx + 1])) / 3;
+                if (xx == 0 || xx == infoBase->height - 1)
+                    continue;
+                int a = (redFun(prevLineBase[xx - 1]) + blueFun(prevLineBase[xx - 1])
+                         + greenFun(prevLineBase[xx - 1])) / 3;
+                int b = (redFun(prevLineBase[xx]) + blueFun(prevLineBase[xx])
+                         + greenFun(prevLineBase[xx])) / 3;
+                int c = (redFun(prevLineBase[xx + 1]) + blueFun(prevLineBase[xx + 1])
+                         + greenFun(prevLineBase[xx + 1])) / 3;
 
-                    int d = (blueFun(lineBase[xx - 1]) + greenFun(lineBase[xx - 1])
-                             + redFun(lineBase[xx - 1])) / 3;
-                    int e = (blueFun(lineBase[xx]) + greenFun(lineBase[xx])
-                             + redFun(lineBase[xx])) / 3;
-                    int f = (blueFun(lineBase[xx + 1]) + greenFun(lineBase[xx + 1])
-                             + redFun(lineBase[xx + 1])) / 3;
+                int d = (blueFun(lineBase[xx - 1]) + greenFun(lineBase[xx - 1])
+                         + redFun(lineBase[xx - 1])) / 3;
+                int e = (blueFun(lineBase[xx]) + greenFun(lineBase[xx])
+                         + redFun(lineBase[xx])) / 3;
+                int f = (blueFun(lineBase[xx + 1]) + greenFun(lineBase[xx + 1])
+                         + redFun(lineBase[xx + 1])) / 3;
 
-                    int g = (greenFun(nextLineBase[xx - 1]) + redFun(nextLineBase[xx - 1])
-                             + blueFun(nextLineBase[xx - 1])) / 3;
-                    int h = (greenFun(nextLineBase[xx]) + redFun(nextLineBase[xx])
-                             + blueFun(nextLineBase[xx])) / 3;
-                    int i = (greenFun(nextLineBase[xx + 1]) + redFun(nextLineBase[xx + 1])
-                             + blueFun(nextLineBase[xx + 1])) / 3;
+                int g = (greenFun(nextLineBase[xx - 1]) + redFun(nextLineBase[xx - 1])
+                         + blueFun(nextLineBase[xx - 1])) / 3;
+                int h = (greenFun(nextLineBase[xx]) + redFun(nextLineBase[xx])
+                         + blueFun(nextLineBase[xx])) / 3;
+                int i = (greenFun(nextLineBase[xx + 1]) + redFun(nextLineBase[xx + 1])
+                         + blueFun(nextLineBase[xx + 1])) / 3;
 
-                    int matrix[3][3] = {a, b, c, d, e, f, g, h, i};
+                int matrix[3][3] = {a, b, c, d, e, f, g, h, i};
 
-                    int sumx = 0;
-                    int sumy = 0;
+                int sumx = 0;
+                int sumy = 0;
 
-                    for (int s = 0; s < 3; s++) {
-                        for (int t = 0; t < 3; t++) {
-                            sumx = sumx + (matrix[s][t] * kx[s][t]);
-                            sumy = sumy + (matrix[s][t] * ky[s][t]); /* use ky, not kx */
-                        }
+                for (int s = 0; s < 3; s++) {
+                    for (int t = 0; t < 3; t++) {
+                        sumx = sumx + (matrix[s][t] * kx[s][t]);
+                        sumy = sumy + (matrix[s][t] * ky[s][t]); /* use ky, not kx */
                     }
-
-                    int newValue = rgb_clamp(sqrt(pow(sumx, 2) + pow(sumy, 2)));
-
-                    // set the new pixel back in
-                    lineToChange[xx] = (lineBase[xx] & 0xFF000000) |
-                                       ((newValue << 16) & 0x00FF0000) |
-                                       ((newValue << 8) & 0x0000FF00) |
-                                       (newValue & 0x000000FF);
                 }
+
+                int newValue = rgb_clamp(sqrt(pow(sumx, 2) + pow(sumy, 2)));
+
+                // set the new pixel back in
+                lineToChange[xx] = (lineBase[xx] & 0xFF000000) |
+                                   ((newValue << 16) & 0x00FF0000) |
+                                   ((newValue << 8) & 0x0000FF00) |
+                                   (newValue & 0x000000FF);
             }
         }
     }
